81d23ea79c2707e7251c8ba935ce8c0eade47580
Hayanobu Nakayama <hnakayam@c1r1s8.42tokyo.jp>
2024-07-16 01:54:10 +0900

    first

---
+++ pipex.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hnakayam <hnakayam@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/15 15:53:56 by hnakayam          #+#    #+#             */
+/*   Updated: 2024/07/16 01:52:24 by hnakayam         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+void	child_one(t_pipex *info, char **environ)
+{
+	char	*cmd[3] = {"grep", "pipe", NULL};
+	close(info->fds[0]);
+	dup2(info->fds[1], 1);
+	dup2(info->fd_in, 0);
+	close(info->fds[1]);
+	close(info->fd_in);
+	execve("/usr/bin/grep", cmd, environ);
+}
+
+int	main(int argc, char *argv[], char **environ)
+{
+	t_pipex	info;
+
+	if (argc != 3)
+		exit(1);
+	info.fd_in = open(argv[1], O_RDONLY);
+	if (info.fd_in < 0)
+		error("open");
+	info.fd_out = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0777);
+	if (info.fd_out < 0)
+		error("open");
+	if (pipe(info.fds) < 0)
+		error("pipe");
+	info.child1 = fork();
+	if (info.child1 == 0)
+		child_one(&info, environ);
+	else
+		close(info.fds[1]);
+	waitpid(info.child1, &info.status, 0);
+	char *cmd2[3] = {"wc", "-l", NULL};
+	dup2(info.fds[0], 0);
+	dup2(info.fd_out, 1);
+	close(info.fds[0]);
+	close(info.fd_out);
+	execve("/usr/bin/wc", cmd2, environ);
+}
---
+++ sample_pipex.h
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sample_pipex.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hnakayam <hnakayam@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/15 15:50:21 by hnakayam          #+#    #+#             */
+/*   Updated: 2024/07/16 01:45:59 by hnakayam         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SAMPLE_PIPEX_H
+# define SAMPLE_PIPEX_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+
+typedef struct s_pipex
+{
+	int		fd_in;
+	int		fd_out;
+	int		fds[2];
+	pid_t	child1;
+	pid_t	child2;
+	int		status;
+}	t_pipex;
+
+// void	error(char *str);
+void error(const char *msg);
+
+#endif
